077ba81e2bd369a86fa41fc295e58c4e452d1968
yashkhem1 <yashkhem1@gmail.com>
2019-09-30 08:56:23 +0530

    Updated code for testing model

---
+++ test_model.py
@@ -0,0 +1,82 @@
+import torch
+import sys
+import cv2
+from code.utils import normalization,denormalize,scale_down, scale_up
+import numpy as np
+from code.loss_utils import resize2d
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+bestModel = sys.argv[1]
+testImage = sys.argv[2]
+modelType = sys.argv[3]
+
+
+testImage =  np.array(cv2.imread(testImage))
+cv2.imwrite( './testInput.jpeg',np.array(testImage).reshape(testImage.shape[0], testImage.shape[1], 3))
+testImage = torch.from_numpy(np.array(testImage).reshape(1,3,testImage.shape[0],testImage.shape[1])).float()
+testImage = scale_down(testImage).to(device)
+print(testImage)
+output=None
+if modelType == 'p':
+    if torch.cuda.is_available():
+        model = torch.load("./P2Pmodel/" + bestModel)
+        gen = model.G.to(device)
+    else:
+        model = torch.load("./P2Pmodel/" + bestModel, map_location='cpu')
+        gen = model.G
+    output = gen.forward(testImage)
+elif modelType == 'c':
+    if torch.cuda.is_available():
+        model = torch.load("./CGmodel/" + bestModel)
+        gen = model.G_XtoY.to(device)
+    else:
+        model = torch.load("./CGmodel/" + bestModel, map_location='cpu')
+        gen = model.G_XtoY
+    output = gen.forward(testImage)
+
+elif modelType=='srn':
+    inp_pred = testImage
+    if torch.cuda.is_available():
+        model = torch.load("./SRNmodel/" + bestModel)
+        torch.set_grad_enabled(False)
+        n, c, h, w = testImage.shape
+        # pred_list = []
+
+        for i in range(model.n_levels):
+            scale = model.scale ** (model.n_levels - i - 1)
+            hi = int(round(h * scale))
+            wi = int(round(w * scale))
+            inp_blur = resize2d(testImage, (hi, wi))
+            inp_pred = resize2d(inp_pred, (hi, wi)).detach()
+            inp_all = torch.cat([inp_blur, inp_pred], 1)  ##Concatenating along the color channels
+            inp_pred = model.SRN_block(inp_all).to(device)
+            del inp_blur, inp_all
+            # pred_list.append(inp_pred)
+
+
+        torch.set_grad_enabled(True)
+    else:
+        model = torch.load("./SRNmodel/" + bestModel, map_location='cpu')
+        torch.set_grad_enabled(False)
+        n, c, h, w = testImage.shape
+        # pred_list = []
+
+        for i in range(model.n_levels):
+            scale = model.scale ** (model.n_levels - i - 1)
+            hi = int(round(h * scale))
+            wi = int(round(w * scale))
+            inp_blur = resize2d(testImage, (hi, wi))
+            inp_pred = resize2d(inp_pred, (hi, wi)).detach()
+            inp_all = torch.cat([inp_blur, inp_pred], 1)  ##Concatenating along the color channels
+            inp_pred = model.SRN_block(inp_all).to(device)
+            del inp_blur, inp_all
+            # pred_list.append(inp_pred)
+
+
+        torch.set_grad_enabled(True)
+
+    output=inp_pred
+
+
+cv2.imwrite( './testOutput.jpeg',np.array(scale_up(output).cpu().detach()).reshape(testImage.shape[2], testImage.shape[3], 3))
