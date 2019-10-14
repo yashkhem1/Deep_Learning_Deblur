0fbeab8fe76c18632a31fe7fc32e4b3af1872c31
yashkhem1 <yashkhem1@gmail.com>
2019-10-14 16:56:44 +0530

    Updated loss

--- code/loss_utils.py
+++ code/loss_utils.py
@@ -27,13 +27,16 @@ def resize2d(img, size):
     return F.interpolate(img, size,mode='bilinear')
 
 def multi_scale_loss (real_im, out_list, nlevels):
-    loss_total = 0
+    # loss_total = 0
     for i in range(nlevels):
         height = out_list[i].shape[2]
         weight = out_list[i].shape[3]
         real_i = resize2d(real_im,(height,weight))
         pred_i = out_list[i]
-        loss = torch.mean((real_i - pred_i)**2)
-        loss_total+= loss
+        if i==0:
+            loss = F.mse_loss(real_i,pred_i)
+        else:
+            loss += F.mse_loss(real_i,pred_i)
 
-    return loss_total
+
+    return loss
