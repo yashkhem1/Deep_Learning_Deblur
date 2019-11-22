bdd7c7b2275a9ae9a2d4cc1f39e170f9e76ef331
Arpit Aggarwal <32244662+arpit-1110@users.noreply.github.com>
2019-11-22 22:30:01 +0000

    added funtion to plot loss

--- code/plot_loss.py
+++ code/plot_loss.py
@@ -1,44 +1,19 @@
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 import pickle
-import os
 import sys
 
-if sys.argv[1] == 'c':
-    modelNumber = sys.argv[2]
-    loss_dir = os.path.join("..", "CGloss")
 
-    loss_DX_f = loss_dir + "/lossD_X" + modelNumber +".pk"
-    loss_DY_f = loss_dir + "/lossd_Y" + modelNumber +".pk"
-    loss_G_f = loss_dir + "/lossG" + modelNumber +".pk"
+with open(sys.argv[1], 'rb') as f:
+	loss = pickle.load(f)
 
-    loss_DX = pickle.load(open(loss_DX_f,'rb'))
-    loss_DY = pickle.load(open(loss_DY_f,'rb'))
-    loss_G = pickle.load(open(loss_G_f,'rb'))
+# print(loss)
 
-    # print(loss_DX)
-    plt.plot([x+1 for x in range(len(loss_DX))], loss_DX)
-    plt.savefig('loss_Dx.png')
-    plt.plot([x+1 for x in range(len(loss_DY))], loss_DY)
-    plt.savefig('loss_Dy.png')
-    plt.plot([x+1 for x in range(len(loss_G))], loss_G)
-    plt.gca().legend(('Discriminator_X loss','Discriminator_Y loss', 'Generator loss'))
-    plt.savefig('loss_G.png')
+import matplotlib.pyplot as plt
+import numpy as np
 
-if sys.argv[1] == 'p':
-    modelNumber = sys.argv[2]
-    loss_dir = os.path.join("..", "P2Ploss")
+loss = np.array(loss)
+loss = loss[:500]
 
-    loss_D_f = loss_dir + "/lossD" + modelNumber +".pk"
-    loss_G_f = loss_dir + "/lossG" + modelNumber +".pk"
+idx = np.arange(len(loss))
 
-    loss_D = pickle.load(open(loss_D_f,'rb'))
-    loss_G = pickle.load(open(loss_G_f,'rb'))
-
-    # print(loss_DX)
-    plt.plot([x+1 for x in range(len(loss_D))], loss_D)
-    plt.savefig('loss_Dx.png')
-    plt.plot([x+1 for x in range(len(loss_G))], loss_G)
-    plt.gca().legend(('Discriminator loss', 'Generator loss'))
-    plt.savefig('loss_G.png')
+plt.plot(idx, loss)
+plt.savefig(sys.argv[2])
