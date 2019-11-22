7706a312cb77e88afe7970ff3c25a0d281d8d770
yashkhem1 <yashkhem1@gmail.com>
2019-11-23 00:40:10 +0530

    Cleaned Code 2.0

--- code/loss_utils.py
+++ code/loss_utils.py
@@ -5,22 +5,6 @@ from torch.autograd import Variable
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def real_mse_loss(D_out):
-    loss = nn.MSELoss()
-    target_tensor = 1.0
-    return loss(D_out, torch.tensor(target_tensor).expand_as(D_out).to(device))
-    # return torch.mean((D_out-1)**2)
-
-def fake_mse_loss(D_out):
-    loss = nn.MSELoss()
-    target_tensor = 0.0
-    return loss(D_out, torch.tensor(target_tensor).expand_as(D_out).to(device))
-    # return torch.mean(D_out**2)
-
-def cycle_consistency_loss  (real_im, reconstructed_im, lambda_weight):
-    reconstructed_loss = torch.mean(torch.abs(real_im - reconstructed_im))
-    return lambda_weight*reconstructed_loss
-
 def resize2d(img, size):
     if len(list(img.shape)) ==3:
         img = img.reshape(img.shape[0],1,img.shape[1],img.shape[2])
